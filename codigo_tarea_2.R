8b13d4b2073b50853dcc3364a263b005c1bacd09
francisco fernandez <francisco.fernandez@uc.cl>
2024-04-11 23:01:56 -0400

    se avanza en el chamullo de la 1 y en los modelos de la 2

--- codigo_tarea_2.R
+++ codigo_tarea_2.R
@@ -8,12 +8,12 @@ library(bookdown)
 pi <-c(46.8,46.4,46.9,45.1,43.8,43.3,45.0,47.2)
 Ni <- c(1183,1510,1597,1924,1178,1324,2173,845)
 yi <- c(554,701,749,868,516,573,978,399)
-#priori A=100 y B=100
+#priori A=5 y B=10
 model_texto <- "model{
     #hiperparametros
     #A es la varianza de mu
-    A <- 100
-    B <- 100
+    A <- 10
+    B <- 10
     
     #priori
     mu ~ dnorm(0,1/A)
@@ -50,143 +50,12 @@ lines(density(mcmc_samples[[1]][,7]), col = "deeppink1")
 lines(density(mcmc_samples[[1]][,8]), col = "indianred1")
 
 
-##otras prioris
-#priori A=1 y B=1
-model_texto <- "model{
-    #hiperparametros
-    #A es la varianza de mu
-    A <- 1
-    B <- 1
-    
-    #priori
-    mu ~ dnorm(0,1/A)
-    sigma ~ dunif(0,B)
-    
-    for (i in 1:N){
-       yi[i] ~ dbin(pi[i], Ni[i]) 
-       theta[i]~dnorm(mu,1/sigma^2)
-       pi[i] <- exp(theta[i])/(1+exp(theta[i]))
-    }
-
-}"
-
-model_data <- list(Ni = Ni,
-                   yi = yi, N = 8)
-
-model <- jags.model(textConnection(model_texto), 
-                    data = model_data,n.chains = 1
-                    ,n.adapt = 500)
-
-mcmc_samples= coda.samples(model , 
-                           variable.names=c("pi")
-                           ,n.iter=6000 )
-summary(mcmc_samples)
-
-plot(density(mcmc_samples[[1]][,1]),xlim=c(0.395,0.55)
-     ,ylim=c(0,60))
-lines(density(mcmc_samples[[1]][,2]), col = "red")
-lines(density(mcmc_samples[[1]][,3]), col = "#0000CD")
-lines(density(mcmc_samples[[1]][,4]), col = "burlywood3")
-lines(density(mcmc_samples[[1]][,5]), col = "chocolate2")
-lines(density(mcmc_samples[[1]][,6]), col = "#458B00")
-lines(density(mcmc_samples[[1]][,7]), col = "deeppink1")
-lines(density(mcmc_samples[[1]][,8]), col = "indianred1")
-
-##otras prioris
-#priori A=1 y B=100
-model_texto <- "model{
-    #hiperparametros
-    #A es la varianza de mu
-    A <- 1
-    B <- 100
-    
-    #priori
-    mu ~ dnorm(0,1/A)
-    sigma ~ dunif(0,B)
-    
-    for (i in 1:N){
-       yi[i] ~ dbin(pi[i], Ni[i]) 
-       theta[i]~dnorm(mu,1/sigma^2)
-       pi[i] <- exp(theta[i])/(1+exp(theta[i]))
-    }
-
-}"
-
-model_data <- list(Ni = Ni,
-                   yi = yi, N = 8)
-
-model <- jags.model(textConnection(model_texto), 
-                    data = model_data,n.chains = 1
-                    ,n.adapt = 500)
-
-mcmc_samples= coda.samples(model , 
-                           variable.names=c("pi")
-                           ,n.iter=6000 )
-summary(mcmc_samples)
-
-plot(density(mcmc_samples[[1]][,1]),xlim=c(0.395,0.525)
-     ,ylim=c(0,60))
-lines(density(mcmc_samples[[1]][,2]), col = "red")
-lines(density(mcmc_samples[[1]][,3]), col = "#0000CD")
-lines(density(mcmc_samples[[1]][,4]), col = "burlywood3")
-lines(density(mcmc_samples[[1]][,5]), col = "chocolate2")
-lines(density(mcmc_samples[[1]][,6]), col = "#458B00")
-lines(density(mcmc_samples[[1]][,7]), col = "deeppink1")
-lines(density(mcmc_samples[[1]][,8]), col = "indianred1")
-
-
-
-#A=100 y B=1
-model_texto <- "model{
-    #hiperparametros
-    #A es la varianza de mu
-    A <- 100
-    B <- 1
-    
-    #priori
-    mu ~ dnorm(0,1/A)
-    sigma ~ dunif(0,B)
-    
-    for (i in 1:N){
-       yi[i] ~ dbin(pi[i], Ni[i]) 
-       theta[i]~dnorm(mu,1/sigma^2)
-       pi[i] <- exp(theta[i])/(1+exp(theta[i]))
-    }
-
-}"
-
-model_data <- list(Ni = Ni,
-                   yi = yi, N = 8)
-
-model <- jags.model(textConnection(model_texto), 
-                    data = model_data,n.chains = 1
-                    ,n.adapt = 500)
-
-mcmc_samples= coda.samples(model , 
-                           variable.names=c("pi")
-                           ,n.iter=6000 )
-summary(mcmc_samples)
-
-plot(density(mcmc_samples[[1]][,1]),xlim=c(0.395,0.525)
-     ,ylim=c(0,60))
-lines(density(mcmc_samples[[1]][,2]), col = "red")
-lines(density(mcmc_samples[[1]][,3]), col = "#0000CD")
-lines(density(mcmc_samples[[1]][,4]), col = "burlywood3")
-lines(density(mcmc_samples[[1]][,5]), col = "chocolate2")
-lines(density(mcmc_samples[[1]][,6]), col = "#458B00")
-lines(density(mcmc_samples[[1]][,7]), col = "deeppink1")
-lines(density(mcmc_samples[[1]][,8]), col = "indianred1")
-
-#aun no se como elegir
-
-plot(density(mcmc_samples[[1]]),xlim=c(0.395,0.525)
-     ,ylim=c(0,60))
 #2---- 
 model_texto2 <- "model{
     #hiperparametros
     #A es la varianza de mu
-    A <- 100
-    B <- 1
+    A <- 10
+    B <- 10
     
     #priori
     mu ~ dnorm(0,1/A)
@@ -211,6 +80,13 @@ mcmc_samples2= coda.samples(model2 ,
                            ,n.iter=6000 )
 summary(mcmc_samples2)
 plot(density(mcmc_samples2[[1]][,1]))
+lines(density(mcmc_samples[[1]][,2]), col = "red",lty=2,lwd=0.5)
+lines(density(mcmc_samples[[1]][,3]), col = "#0000CD",lty=2,lwd=0.5)
+lines(density(mcmc_samples[[1]][,4]), col = "burlywood3",lty=2,lwd=0.5)
+lines(density(mcmc_samples[[1]][,5]), col = "chocolate2",lty=2,lwd=0.5)
+lines(density(mcmc_samples[[1]][,6]), col = "#458B00",lty=2,lwd=0.5)
+lines(density(mcmc_samples[[1]][,7]), col = "deeppink1",lty=2,lwd=0.5)
+lines(density(mcmc_samples[[1]][,8]), col = "indianred1",lty=2,lwd=0.5)
 
 
 
@@ -221,8 +97,8 @@ yi <- c(554,701,749,868,516,573,978,399)
 model_texto3 <- "model{
     #hiperparametros
     #C es la varianza de theta[1]
-    C <- 1
-    D <- 1
+    C <- 0.00000001
+    D <- 0.000000001
     
     #priori
     theta[1] ~ dnorm(0,1/C)
@@ -248,8 +124,7 @@ mcmc_samples3= coda.samples( model3 ,
                             ,n.iter=6000 )
 summary(mcmc_samples3)
 
-plot(density(mcmc_samples3[[1]][,1]),xlim=c(0.395,0.525)
-     ,ylim=c(0,60))
+plot(density(mcmc_samples3[[1]][,1]))
 lines(density(mcmc_samples3[[1]][,2]), col = "red")
 lines(density(mcmc_samples3[[1]][,3]), col = "#0000CD")
 lines(density(mcmc_samples3[[1]][,4]), col = "burlywood3")
